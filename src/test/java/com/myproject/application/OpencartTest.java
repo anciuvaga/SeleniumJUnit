c2a43bbd27c7d8009b097ba35ddad66147814521
Andrei Ciuvaga <andrei.ciuvaga@gmail.com>
2019-12-01 23:16:08 +0200

    First Commit

---
+++ src/test/java/com/myproject/application/OpencartTest.java
@@ -0,0 +1,20 @@
+package com.myproject.application;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class OpencartTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
